1e0522007d58cdee67d1e4a3bb5cd55d317f730b
mollyfm12 <mollyfm@email.sc.edu>
2025-03-30 16:57:40 -0400

    deploy8

--- italy-blog-app/src/index.js
+++ italy-blog-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './index.css';
 import Layout from './Layout';
 import Home from './pages/Home';
