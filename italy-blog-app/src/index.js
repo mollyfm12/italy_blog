1bb964115abd31278c8b11360fe4816fc228ee13
mollyfm12 <mollyfm@email.sc.edu>
2025-03-30 17:01:54 -0400

    deploy9

--- italy-blog-app/src/index.js
+++ italy-blog-app/src/index.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Layout from './Layout';
 import Home from './pages/Home';
+import Nopage from './pages/Nopage';
 
 function App() {
-  return(
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Routes path="/" element={<Layout />}>
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="About" element={<About />} />
-          <Route path="Blog" element={<Blog />} />
           <Route path="*" element={<Nopage />} />
-        </Routes>
-      </BrowserRouter>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
-};
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root')); //accessing the root from html to push into there
-root.render( //this is where you type HTML because you are rendering
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-
