24935001e34a3fcf91721d46498a30e4457b9a17
mollyfm12 <mollyfm@email.sc.edu>
2025-03-30 23:49:41 -0400

    Updates

---
+++ precache-manifest.a2a81a6576ed9e2fd47996c34ebcb539.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "465bc1116d918e06427570e85450b7a2",
+    "url": "/italy_blog/index.html"
+  },
+  {
+    "revision": "76a0095a95c721c17487",
+    "url": "/italy_blog/static/css/main.08f1d538.chunk.css"
+  },
+  {
+    "revision": "88e918333f8c4a49e841",
+    "url": "/italy_blog/static/js/2.8d9fb257.chunk.js"
+  },
+  {
+    "revision": "8db1e5edfff3ff6d324aa575732ed90f",
+    "url": "/italy_blog/static/js/2.8d9fb257.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76a0095a95c721c17487",
+    "url": "/italy_blog/static/js/main.d6ec3ebe.chunk.js"
+  },
+  {
+    "revision": "737f26303d39543f12b3",
+    "url": "/italy_blog/static/js/runtime-main.ffcbf4f7.js"
+  },
+  {
+    "revision": "35cdc5fd9984e1663d5d9e0edc95e73a",
+    "url": "/italy_blog/static/media/IMG_5234.35cdc5fd.jpg"
+  },
+  {
+    "revision": "0e505189ae852808d0e33a8ca0fef143",
+    "url": "/italy_blog/static/media/IMG_5239.0e505189.jpg"
+  },
+  {
+    "revision": "4f04b5bcc7592ecfcea32152c4f34d02",
+    "url": "/italy_blog/static/media/IMG_5250.4f04b5bc.jpg"
+  },
+  {
+    "revision": "a24822c61c3ef952d2798955163642e3",
+    "url": "/italy_blog/static/media/capp.a24822c6.png"
+  },
+  {
+    "revision": "085060f1d2457cfd5efbd3ccc41b005d",
+    "url": "/italy_blog/static/media/coll.085060f1.JPG"
+  },
+  {
+    "revision": "663b05c56f1b5d3958c5b326653e334b",
+    "url": "/italy_blog/static/media/cook.663b05c5.JPG"
+  },
+  {
+    "revision": "622316e490d508f41be920e901a338c5",
+    "url": "/italy_blog/static/media/culture.622316e4.png"
+  },
+  {
+    "revision": "2eee91da8350426af855061c726c2690",
+    "url": "/italy_blog/static/media/david.2eee91da.png"
+  },
+  {
+    "revision": "6f5efbb64204e8436944e51c727174e6",
+    "url": "/italy_blog/static/media/dumo-close.6f5efbb6.png"
+  },
+  {
+    "revision": "f08aa3831b5545a02529e4c90402793c",
+    "url": "/italy_blog/static/media/duomo.f08aa383.jpg"
+  },
+  {
+    "revision": "79dc5ed13f7920fac2428b3570251358",
+    "url": "/italy_blog/static/media/flight.79dc5ed1.JPG"
+  },
+  {
+    "revision": "189fcc4e994fc0216faef0b017497707",
+    "url": "/italy_blog/static/media/food.189fcc4e.png"
+  },
+  {
+    "revision": "89b452030dda3d8dca115f4e0ea9fcec",
+    "url": "/italy_blog/static/media/leather.89b45203.png"
+  },
+  {
+    "revision": "86f2293a8467bca43598e81bcac53f9e",
+    "url": "/italy_blog/static/media/main.86f2293a.png"
+  },
+  {
+    "revision": "afb2527e044c9a6bc4c866d11b6da4ab",
+    "url": "/italy_blog/static/media/medici.afb2527e.jpg"
+  },
+  {
+    "revision": "224331d66d13b23da57d04332a86be97",
+    "url": "/italy_blog/static/media/plaza.224331d6.png"
+  },
+  {
+    "revision": "f22b0d874b506f5fe194e14fe4f41492",
+    "url": "/italy_blog/static/media/ponte.f22b0d87.png"
+  },
+  {
+    "revision": "a71590ba8704d477385dea5564bc1f3e",
+    "url": "/italy_blog/static/media/square.a71590ba.png"
+  },
+  {
+    "revision": "aa1cb6eeddbaea280f0c35e690572bb3",
+    "url": "/italy_blog/static/media/street-duomo.aa1cb6ee.JPG"
+  },
+  {
+    "revision": "463ce7bebd992699d91bf259f1c073ac",
+    "url": "/italy_blog/static/media/sunset.463ce7be.png"
+  },
+  {
+    "revision": "37331bcd0475ba669042312f1b00881b",
+    "url": "/italy_blog/static/media/vatican.37331bcd.png"
+  },
+  {
+    "revision": "658b8198b00860772584cc939a79308b",
+    "url": "/italy_blog/static/media/wwi.658b8198.jpg"
+  }
+]);
